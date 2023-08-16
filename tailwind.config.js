8995ab22639ca55f2a5ca4910986c9a2178a887f
yves173 <ykwizera67@gmail.com>
2023-08-16 16:44:54 +0200

    update main  start-up for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html', './build/sections/*.html', './build/js/*.js'],
+  content: ["./*.html"],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem'
-      }
-    },
-    extend: {
-      colors: {
-        'primary-green': '#8ACC50',
-      },
-    },
-    fontFamily: {
-      'roboto': 'Roboto, sans-serif',
-      'raleway': 'raleway, Helvetica',
-      'montserrat': 'Montserrat, Helvetica'
-    }
+    extend: {},
   },
   plugins: [],
 }
